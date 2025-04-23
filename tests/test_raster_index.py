59a2da2e25e4169a9db5fab1ace23dffe5c6ecdc
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-04-22 21:17:57 -0400

    Add assign_raster_index to public API

--- tests/test_raster_index.py
+++ tests/test_raster_index.py
@@ -3,24 +3,13 @@ import rioxarray  # noqa
 import xarray as xr
 from affine import Affine
 
-from rasterix import RasterIndex
-
-
-def set_raster_index(obj):
-    x_dim = obj.rio.x_dim
-    y_dim = obj.rio.y_dim
-
-    index = RasterIndex.from_transform(
-        obj.rio.transform(), obj.sizes[x_dim], obj.sizes[y_dim], x_dim=x_dim, y_dim=y_dim
-    )
-    coords = xr.Coordinates.from_xindex(index)
-    return obj.assign_coords(coords)
+from rasterix import RasterIndex, assign_raster_index
 
 
 def test_rectilinear():
     source = "/vsicurl/https://noaadata.apps.nsidc.org/NOAA/G02135/south/daily/geotiff/2024/01_Jan/S_20240101_concentration_v3.0.tif"
     da_no_raster_index = xr.open_dataarray(source, engine="rasterio")
-    da_raster_index = set_raster_index(da_no_raster_index)
+    da_raster_index = assign_raster_index(da_no_raster_index)
     assert da_raster_index.equals(da_no_raster_index)
 
 
@@ -31,7 +20,7 @@ def test_sel_slice():
     ds = xr.Dataset({"foo": (("y", "x"), np.ones((10, 12)))})
     transform = Affine.identity()
     ds = ds.rio.write_transform(transform)
-    ds = set_raster_index(ds)
+    ds = assign_raster_index(ds)
 
     assert ds.xindexes["x"].transform() == transform
 
